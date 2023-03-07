d0b313544174bd9aee28744ca0bb3f377811c21a
Teklay <teklay@gmail.com>
2023-03-07 06:34:05 +0300

    Add access modifier for methods in person class

--- person.rb
+++ person.rb
@@ -9,12 +9,14 @@ class Person
     attr_reader :id
     attr_accessor :name, :age
 
-    private def is_of_age?
+    def is_of_age?
         @age >= 18
     end
+    private :is_of_age?
 
     def can_use_services?
         is_of_age? || @parent_permission
     end
+    public :can_use_services?
 
 end
