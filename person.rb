7d26caf548b64f7c2426f418c27fe9d1cfa8e67b
Teklay <teklay@gmail.com>
2023-03-07 07:06:23 +0300

    Fix Linters

--- person.rb
+++ person.rb
@@ -1,22 +1,21 @@
 class Person
-    def initialize(name = "Unknown", age, parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    attr_reader :id
-    attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def is_of_age?
-        @age >= 18
-    end
-    private :is_of_age?
+  def of_age?
+    @age >= 18
+  end
+  private :of_age?
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
-    public :can_use_services?
-
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+  public :can_use_services?
 end
--- student.rb
+++ student.rb
@@ -1,16 +1,15 @@
 require_relative 'person'
 
-#student inherits from person
+# student inherits from person
 class Student < Person
+  # constructor
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
-    #constructor
-    def initialize(age, name = "Unknown", parent_permission = true,classroom)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
-
-    #Mehod to play
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  # Mehod to play
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
