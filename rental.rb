a382105c94d8d4c78ab1fec83bd228ec5e9ba0cb
Teklay <teklay@gmail.com>
2023-03-08 05:17:10 +0300

    Create relationship between Person and Book using the intermediate class Rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
-    attr_accessor :date
-    
+    attr_accessor :date, :person, :book
+
     def initialize(date)
         @date = date
+        @person = person
+        person.rentals.push(self)
+
+        @book = book
+        book.rentals.push(self)
     end
 end
