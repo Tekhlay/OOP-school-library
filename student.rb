310e87aa756d7bbfe85724d4567b150cef06df6e
Teklay <teklay@gmail.com>
2023-03-08 05:06:31 +0300

    Create relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -2,14 +2,23 @@ require_relative 'person'
 
 # student inherits from person
 class Student < Person
+  attr_reader :classroom
+  attr_accessor :type
   # constructor
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true, type='Student')
     super(age, name, parent_permission)
     @classroom = classroom
+    @type = type
   end
+
 
   # Mehod to play
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
